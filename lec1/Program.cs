5c8b1ff0c96ee655aafdce66591a22871ed554eb
Stanislava <ivanenko.stasy@gmail.com>
2022-09-14 21:51:36 +0300

    work with int

--- lec1/Program.cs
+++ lec1/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("New language!!!");
+int ain = 10;
+int bin = 6;
+int resalt = ain/bin;
+Console.Write(resalt);
